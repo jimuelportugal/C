6a635ddccd42b2978ea82b44588fd56bb7a3a07e
Taji Sparrow <55313538+jimuelportugal@users.noreply.github.com>
2024-08-28 09:23:54 +0800

    Create main.c
    Start

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("welcome to the college of Computer Studies \n");
+    printf("I'am JIMUEL PORTUGAL \t");
+
+    int my_variable_1;
+    my_variable_1 = 10.5;
+
+    printf("%d", my_variable_1);
+    printf("%f", my_variable_1);
+
+
+    return 0;
+}
