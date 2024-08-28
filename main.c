f2661e92dd7abe38153ca4f63d31597f5c9294f3
Taji Sparrow <55313538+jimuelportugal@users.noreply.github.com>
2024-08-28 09:42:35 +0800

    Update main.c

--- main.c
+++ main.c
@@ -4,13 +4,24 @@
 int main()
 {
     printf("welcome to the college of Computer Studies \n");
-    printf("I'am JIMUEL PORTUGAL \t");
+    printf("I'am JIMUEL PORTUGAL \n");
 
     int my_variable_1;
-    my_variable_1 = 10.5;
+    my_variable_1 = 10;
+    int my_variable_2;
+    my_variable_2 = 20;
+
+    int total;
+    total = my_variable_1 + my_variable_2;
 
     printf("%d", my_variable_1);
-    printf("%f", my_variable_1);
+    printf("+\n");
+    printf("%d", my_variable_2);
+    printf("\n");
+
+    printf("= \t");
+
+    printf("%d", total);
 
 
     return 0;
