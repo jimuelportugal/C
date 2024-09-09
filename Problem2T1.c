7583c1a89ae76f938759761a212f9b5d2d6068fa
Taji Sparrow <55313538+jimuelportugal@users.noreply.github.com>
2024-09-09 09:28:53 +0800

    Update Problem2T1.c

--- Problem2T1.c
+++ Problem2T1.c
@@ -3,7 +3,7 @@
 
 int main() {
     //PROBLEM2
-    float number1, number2;
+    int number1, number2;
     scanf("%f %f",&number1,&number2);
     float Average = (number1 + number2) / 2;
     printf("Average: %f", Average);
