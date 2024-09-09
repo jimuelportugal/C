cc81c9322b421b6570fd62cbde43ae38f262906e
Taji Sparrow <55313538+jimuelportugal@users.noreply.github.com>
2024-09-09 09:10:12 +0800

    Create Problem2T1.c

---
+++ Problem2T1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+int main() {
+    //PROBLEM2
+    float number1, number2;
+    scanf("%f %f",&number1,&number2);
+    float Average = (number1 + number2) / 2;
+    printf("Average: %f", Average);
+    return 0;
+}
