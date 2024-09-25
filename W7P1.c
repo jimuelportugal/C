7f6113710840ff71c90739847352ef79eb6bc376
Taji Sparrow <55313538+jimuelportugal@users.noreply.github.com>
2024-09-25 08:51:14 +0800

    Create W7P1.c

---
+++ W7P1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("----- ***** ----- ***** ----- ***** -----\n");
+    printf("Welcome to Naga College Foundation\n");
+    printf("I am Jimuel Portugal from the \n");
+    printf("College of Computer Studies\n");
+    printf("***** ----- ***** ----- ***** ----- *****\n");
+    printf("Press any key to continue....\n");
+    return 0;
+}
+/*
+ Write a program in C that displays the following output.
+
+----- ***** ----- ***** ----- ***** -----
+Welcome to Naga College Foundation
+I am [Your Name] from the
+College of Computer Studies
+***** ----- ***** ----- ***** ----- *****
+Press any key to continue....
+
+*/
