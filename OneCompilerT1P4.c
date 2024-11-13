d20ab7c00704494a91f20747d617d19b0faeb324
Taji Sparrow <55313538+jimuelportugal@users.noreply.github.com>
2024-11-13 05:37:54 +0430

    Create OneCompilerT1P4.c

---
+++ OneCompilerT1P4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int user;
+
+    printf("Enter a number: ");
+    scanf("%d", &user);
+    printf("Result : %d\n", user * 2);
+
+    return 0;
+}
+/*
+Write a program ask a number. Display the number multiplied by 2.
+
+Input: 10
+Ouput: 20
+*/
